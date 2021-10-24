06b6965cd4653f0b29fe76b384fe444a7010898f
klinkmann <93070399+klinkmann@users.noreply.github.com>
2021-10-24 10:41:35 +0300

    project11

--- main.ts
+++ main.ts
@@ -1,93 +1,22 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . # . # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . # # # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # . # #
-        # . . . #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        # . . . #
-        # . . . #
-        `)
-    basic.showLeds(`
-        . . . . .
-        # # # . .
-        # . . # #
-        # . . . #
-        # . . . #
-        `)
-    basic.showLeds(`
-        . . . . .
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        `)
-    basic.showLeds(`
-        . . # # #
-        # # . . #
-        # . . . #
-        # . . . #
-        # . . . #
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # . . . #
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # . . # #
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # . # # #
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+let _1 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Hello!")
+})
+loops.everyInterval(1000, function () {
+    _1 = randint(0, 10)
+    basic.showString("" + (_1))
+})
+basic.forever(function () {
+    if (_1 == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
 })
